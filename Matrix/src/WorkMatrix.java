7aa77ede7ac54d7391c25f2e75e9407a525dd73f
krafft12 <andreikrafft@gmail.com>
2015-12-17 09:54:03 +0200

    impare

--- Matrix/src/WorkMatrix.java
+++ Matrix/src/WorkMatrix.java
@@ -35,4 +35,15 @@ public class WorkMatrix {
 		}
 		return par;
 	}
+
+public int imPare(){
+	int impar = 0;
+	for (int i = 0; i < matrix.length; i++) {
+		for (int j = 0; j < matrix.length; j++) {
+			if (matrix[i][j]%2!=0 )
+					impar=impar+1;
+			
+		}
+	}
+	return impar;
 }
