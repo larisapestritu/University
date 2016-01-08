5621d3d6be88da61fb0b7fdfef0f5eb024ac433d
larisapestritu <larisapestritu@yahoo.com>
2016-01-08 18:23:02 +0200

    min,max,par,imp

--- Matrix/src/WorkMatrix.java
+++ Matrix/src/WorkMatrix.java
@@ -1,5 +1,4 @@
-
-
+import java.util.Arrays;
 
 public class WorkMatrix {
 	private int[][] matrix;
@@ -14,6 +13,11 @@ public class WorkMatrix {
 
 	public void setMatrix(int[][] matrix) {
 		this.matrix = matrix;
+	}
+	int[] addElement(int[] org, int added){
+		int[] result = Arrays.copyOf(org, org.length + 1);
+		result[org.length] = added;
+		return result;
 	}
 	public int SumaMatrix(){
 		int s = 0;
@@ -46,4 +50,55 @@ public int imPare(){
 		}
 	}
 	return impar;
-} }
+} 
+
+public int MaxMatrix() {
+	int max = matrix[0][0];
+	for (int i = 0; i < matrix.length; i++) {
+		for (int j = 0; j < matrix.length; j++) {
+			if (max < matrix[i][j])
+				max = matrix[i][j];
+		}
+	} 
+
+	return max;}
+
+
+public int MinMatrix() {
+	int min = matrix[0][0];
+	for (int i = 0; i < matrix.length; i++) {
+		for (int j = 0; j < matrix.length; j++) {
+			if (min > matrix[i][j])
+				min = matrix[i][j];
+		}
+	}
+	return min;}
+
+
+public int[] Odd(int[][] matrix) // afiseaza elementele impare
+{
+	int[] temp = new int[0];
+	for (int i = 0; i < matrix.length; i++) {
+		for (int j = 0; j < matrix.length; j++) {
+			if (matrix[i][j] % 2 != 0) {
+				temp = addElement(temp, matrix[i][j]);
+			}
+		}
+	}
+	return temp;
+}
+
+public int[] Even(int[][] matrix) //afiseaza elementele pare
+{
+	int[] temp = new int[0];
+	for (int i = 0; i < matrix.length; i++) {
+		for (int j = 0; j < matrix.length; j++) {
+			if (matrix[i][j] % 2 == 0) {
+				temp = addElement(temp, matrix[i][j]);
+			}
+		}
+	}
+	return temp;}
+}
+
+
